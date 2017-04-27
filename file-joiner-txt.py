5b60c2bca849ffdd8104be76ddf85ee477db2a92
simonlindgren <simon.lindgren@umu.se>
2017-04-27 12:02:07 +0200

    Initial commit

---
+++ file-joiner-txt.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+"""
+script: txt-joiner.py
+author: Simon Lindgren
+non-standard packages: -
+functionality: joins all txt files in a folder into one big txt file
+"""
+
+import glob
+fs = glob.glob("data/*.txt")                                       # get a list of all files
+
+with open("big.txt", "wb") as outfile:                             # create output file
+    for f in fs:                                                   # iterate over the files
+        with open(f, "rb") as infile:                              # open the file
+            outfile.write(f.encode('ascii')+"\n".encode('ascii'))  # write the filename on one line
+            outfile.write(infile.read()+"\n\n".encode('ascii'))    # write the file content
---
+++ readme.md
@@ -0,0 +1,4 @@
+# txtls [text-tools]
+Python scripts to do stuff with text data.
+
+- **file-joiner-txt.py** joins all txt files in a folder into one big txt file
