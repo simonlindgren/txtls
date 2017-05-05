b47aa127a621ba0237e29c460bc9ac1c7d889604
simonlindgren <simon.lindgren@umu.se>
2017-05-05 07:22:20 +0200

    adding col-grabber

---
+++ col-grabber.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+"""
+script: col-grabber.py
+author: Simon Lindgren
+non-standard packages: pandas
+functionality: creates a txt file with the contents of a selected csv file column
+"""
+
+# Move to your data directory
+import os
+path = './data/'
+os.chdir(path)
+
+# Loop through the csv files
+import pandas as pd
+
+for file in os.listdir(): # for each file in the directory
+    if file.endswith(".csv"): # if it has the csv extension
+        f = open(file, 'r') # open it
+        dataframe = pd.read_csv(f) # into a dataframe
+
+        #headers = dataframe.dtypes.index # view the column names if needed
+        #print(headers)
+
+        small_frame = dataframe[['target']] # extract the column with the name 'target' into a smaller dataframe
+        small_frame.to_csv((file[:-4]+'.txt'), header=None, index=None, sep=' ', mode='a') # write it to a file
+        f.close() # close the file and return to the top of the loop
